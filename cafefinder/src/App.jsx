7beb2bc19d0ed6d470ac94948453b44cf9816312
Ganga Raghuwanshi <gangaraghuwanshi@Gangas-MacBook-Pro.local>
2025-04-29 18:53:57 +0530

    footer has been added to the home  page

--- cafefinder/src/App.jsx
+++ cafefinder/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import CoffeeCarousel from './components/CoffeeCarousel'
 import WhyChooseUs from './components/WhyChooseUs'
 import TestimonialCarousel from './components/TestimonialCarousel'
+import Footer from './components/Footer'
 function App() {
 
 
@@ -11,6 +12,7 @@ function App() {
       <CoffeeCarousel/>
       <WhyChooseUs/>
       <TestimonialCarousel/>
+      <Footer/>
     </>
   )
 }
---
+++ cafefinder/src/components/Footer.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <>
+    <footer className='footer'>
+    <div className="footer-columns">
+    <div className='company-brand'>
+        <h2>Cafe scene</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto praesentium perspiciatis autem, cupiditate, minima harum odit neque cum tempora excepturi et assumenda quae explicabo nesciunt facilis hic obcaecati quasi libero!</p>
+    </div>
+    <div>
+        <h3>Quick links</h3>
+        <ul>
+            <li><a>Home</a></li>
+            <li><a>About us</a></li>
+            <li><a>Contact</a></li>
+            <li><a>Privacy policy</a></li>
+            <li><a>Terms and conditions</a></li>
+        </ul>
+    </div>
+    <div>
+        <h3>Contact</h3>
+        <ul>
+            <li> Email: support@cafefinder.com</li>
+            <li> Phone: +1-234-567-8910</li>
+            <li> Location: Somewhere on Earth</li>
+        </ul>
+    </div>
+    <div>
+        <h3>Follow us</h3>
+        <ul>
+            <li><a>Instagram</a></li>
+            <li><a>Twitter</a></li>
+        </ul>
+    </div>
+
+
+    <div className="footer-bottom">
+        &copy; 2025 CafeFinder. All rights reserved.
+      </div>
+    </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
